7b07d81070b69484b222b5153394389ee9e51ae6
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-09-13 11:26:59 +0300

    com 2

--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -1,23 +1,28 @@
 import { Component } from 'react'
-import {getImage} from '../services'
 import Notiflix from 'notiflix';
+import {getImage} from '../services'
 import {ImageGallery} from 'components/ImageGallery/ImageGallery'
 import {ImageGalleryItem} from 'components/ImageGalleryItem/ImageGalleryItem'
 
 export class ContentInfo extends Component {
   state = {
-    image: ''
+    image: null,
+    isLoading: false,
   }
   componentDidUpdate(prevProps, prevState) {
     if(prevProps.searchText !== this.props.searchText) {
+      this.setState({isLoading: true});
       getImage(this.props.searchText)
         .then((response) => {
           if (response.data.hits.length === 0) {
               Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
               return;
           }
-          this.setState({ image: response.data.hits});
+          this.setState({image: response.data.hits});
 
+        })
+        .finally (() => {
+          this.state({isLoading: false});
         })
         .catch(function(error) {
             Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
@@ -27,9 +32,10 @@ export class ContentInfo extends Component {
   }
   
   render() {
-    const {image} = this.state;
+    const {image, isLoading} = this.state;
     return (
       <>
+        {isLoading && <h1>Loading .....</h1>}
         <ImageGallery>
             {image && image.map((el) => {
               return (
