5d9fceccc211f53094fbc96bb1fc4bdebf326509
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-09-15 18:08:59 +0300

    com 4

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 
-const basicLightbox = require('basiclightbox')
+// const basicLightbox = require('basiclightbox')
+// import * as basicLightbox from 'basiclightbox'
+// import "basiclightbox/dist/basiclightbox.min.css";
 
 export class Modal extends Component {
     state={}
@@ -20,13 +22,13 @@ export class Modal extends Component {
 
     render() {
     const {largeImageURL, closeModal} = this.props
-    console.log(largeImageURL)
+    // console.log(largeImageURL)
     return (
         <div className="Overlay" onClick={closeModal}>
             <div className="Modal">
-                {basicLightbox.create(`
-                    <img src="${largeImageURL}" width="800" height="600">`).show()}
-                {/* <img src={largeImageURL} alt="" width="800" height="600"/> */}
+                {/* {basicLightbox.create(`
+                    <img src="${largeImageURL}" width="800" height="600">`).show()} */}
+                <img src={largeImageURL} alt="" width="800" height="600"/>
             </div>
         </div>
     );
